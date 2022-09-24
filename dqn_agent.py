a981b2e657d8ef3a43a8a1e95f53c6ee3a587eba
Deepakgthomas <thomasdeepak247@gmail.com>
2022-09-24 09:49:23 -0500

    Update dqn_agent.py

--- dqn_agent.py
+++ dqn_agent.py
@@ -6,7 +6,8 @@ import numpy as np
 import tensorflow as tf
 from tensorflow.python.keras import Sequential
 from tensorflow.python.keras.layers import Dense
-from keras.optimizers import Adam
+from tensorflow.keras.optimizers import Adam
+
 
 
 # pylint: disable=too-many-instance-attributes
